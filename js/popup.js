8a7df30b8865dd80b9338b81e1bf894c5815f0cb
Aditya Raj <167434455+immortaladi@users.noreply.github.com>
2024-11-14 11:00:33 +0000

    MAIN

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (!sessionStorage.getItem("popupShown")) {
+      setTimeout(showPopup, 9000); // Show popup after 9 seconds
+    }
+  });
+  
+  function showPopup() {
+    document.getElementById("popup").style.display = "block";
+    sessionStorage.setItem("popupShown", "true"); // Prevent popup from showing again in the same session
+  }
+  
+  function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
